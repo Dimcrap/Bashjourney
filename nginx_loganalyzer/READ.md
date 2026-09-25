4202eac6294431e3b62f6d91699c7510ce5b76f1
mamadreza <mamad.rahmani235@gmail.com>
2026-09-25 12:13:00 -0400

    defining ip counter logic among testing it-its weekend im quitin for now :)

---
+++ nginx_loganalyzer/READ.md
@@ -0,0 +1,5 @@
+
+# 1. Top 5 IP addresses with most requests
+# 2. Top 5 most requested paths
+# 3. Top 5 reponse status codes
+# 4. Top 5 user agents
--- nginx_loganalyzer/analyzer.sh
+++ nginx_loganalyzer/analyzer.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 
+
+#========================= global Variables ==============================
+#top 5 IPs
+ip1=0;ip2=0;ip3=0;ip4=0;ip5=0
+ipval1=0;ipval2=0;ipval3=0;ipval4=0;ipval5=0
+logfile=$1
+
+#********************************************************************************
+
+
+#======================== Functions  ============================================
+
 list_usr()
 {
 	ls /usr
@@ -8,7 +20,44 @@ list_usr()
 	echo "here the script output $outy"
 }
 
-logfile=$1
+count_ip()
+{
+	targetip=$1
+	return $(grep -o ${targetip} ${logfile} | wc -l)
+	
+}
+
+chekcwithtops()
+{
+	targetip=$1
+	if [ $targetip   ];then
+		
+	fi
+	
+	
+	
+}
+
+#compareip
+
+#**********************************************************************************
+
+echo "counting ip 178.128.94.113"
+count_ip "178.128.94.113"
+resvalue=$?
+echo "count result : ${resvalue}"
+
+
+
+
+
+
+
+
+
+
+
+
 
 if [ -z $logfile ]
 then 
